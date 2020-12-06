6f1f80c5cc8ea597e7f81da264e40710efa0d873
adam <adamforsberg94@gmail.com>
2020-12-06 14:52:00 +0100

    Added shorter alternative solution

--- day06/day06.py
+++ day06/day06.py
@@ -1,8 +1,7 @@
 import fileinput
+import functools
 
-def main():
-    temp = open("input6.txt", "r").read().strip().split("\n\n")
-    groups = [group.split("\n") for group in temp]
+def alternativeExplicitVersion(groups):
     ans1 = ans2 = 0
     for group in groups:
         s1 = s2 = set(group[0])
@@ -13,4 +12,12 @@ def main():
         ans1 += len(s1)    
         ans2 += len(s2)
     print(ans1, ans2)
+
+def main():
+    temp = open("input6.txt", "r").read().strip().split("\n\n")
+    groups = [group.split("\n") for group in temp]
+
+    print(sum([len(functools.reduce(lambda a, b: set(a) | set(b), g)) for g in groups]))
+    print(sum([len(functools.reduce(lambda a, b: set(a) & set(b), g)) for g in groups]))    
+
 main()
