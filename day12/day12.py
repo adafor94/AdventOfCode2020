5097e0f2e67c5784e0ac1dbfef1b72de20fc49fc
adam <adamforsberg94@gmail.com>
2020-12-12 18:21:13 +0100

    Redone using imaginary numbers

--- day12/day12.py
+++ day12/day12.py
@@ -1,46 +1,64 @@
 import math
 
-def getAngle(x,y):
-    return math.atan2(y,x) * 180 / math.pi
-
-def getCoordinates(angle, x,y):
-    distance = math.sqrt(x**2 + y**2)
-    newx = math.cos(angle * math.pi / 180) * distance
-    newy = math.sin(angle * math.pi / 180) * distance
-    return (newx,newy)
-
 def main():
     instructions = open("input12.txt", "r").read().strip().split("\n") 
-    instructions = [[instruction[0], instruction[1:]] for instruction in instructions]
-    waypointXpos = 10
-    waypointYpos = 1
 
-    boatXpos = 0
-    boatYpos = 0
+    #PART 1
+    pos = 0+0j
+    direction = 1+0j
 
     for instruction in instructions:
         command = instruction[0]
-        arg     = int(instruction[1])
+        arg     = int(instruction[1:])
 
         if command == 'N':
-            waypointYpos += arg
+            pos += 1j * arg
         elif command == 'S':
-            waypointYpos -= arg
+            pos -= 1j * arg
         elif command == 'E':
-            waypointXpos += arg
+            pos += 1 * arg
         elif command == 'W':
-            waypointXpos -= arg
+            pos -= 1 * arg
         elif command == 'L':
-            oldAngle = getAngle(waypointXpos,waypointYpos)
-            newAngle = oldAngle + arg
-            waypointXpos, waypointYpos = getCoordinates(newAngle, waypointXpos,waypointYpos)
+            steps = int(arg/90)
+            for _ in range(steps):
+                direction *= 1j 
         elif command == 'R':
-            oldAngle = getAngle(waypointXpos,waypointYpos)
-            newAngle = oldAngle - arg
-            waypointXpos, waypointYpos = getCoordinates(newAngle, waypointXpos,waypointYpos)
+            steps = int(arg/90)
+            for _ in range(steps):
+                direction *= -1j
         elif command == 'F':
-            boatXpos += waypointXpos * arg
-            boatYpos += waypointYpos * arg
-    print(abs(boatXpos) + abs(boatYpos))
+            pos += direction * arg
+
+    print("Part1:", abs(pos.real) + abs(pos.imag))
+
+    #PART 2
+    currentPos = 0 + 0j
+    waypoint = 10 + 1j
+
+    for instruction in instructions:
+        command = instruction[0]
+        arg     = int(instruction[1:])
+
+        if command == 'N':
+            waypoint += 1j * arg
+        elif command == 'S':
+            waypoint -= 1j * arg
+        elif command == 'E':
+            waypoint += 1 * arg
+        elif command == 'W':
+            waypoint -= 1 * arg
+        elif command == 'L':
+            steps = int(arg/90)
+            for _ in range(steps):
+                waypoint *= 1j 
+        elif command == 'R':
+            steps = int(arg/90)
+            for _ in range(steps):
+                waypoint *= -1j
+        elif command == 'F':
+            currentPos += waypoint * arg
+
+    print("Part2:", abs(currentPos.real) + abs(currentPos.imag))
 
 main()
