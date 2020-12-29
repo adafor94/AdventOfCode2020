f1141128e8123bc4f56e230fe8e79e63e5376d1a
adam <adamforsberg94@gmail.com>
2020-12-29 16:43:12 +0100

    Day 19

--- day19/day19.py
+++ day19/day19.py
@@ -1,11 +1,11 @@
 def main():
     input = open("input19.txt", "r").read().strip().split('\n\n')
-    rules = input[0].split('\n')
+    allRules = input[0].split('\n')
     messages = input[1].split('\n')
-
     r = {}
 
-    for rule in rules: 
+    parsers = 0
+    for rule in allRules: 
         rule = rule.split(':')
         num = rule[0]
 
@@ -16,16 +16,15 @@ def main():
         else:
             rules = [subrule.strip().split(' ') for subrule in rule[1].split('|')]
             r[num] = Parser(r,rules)
+            parsers += 1
         
-        #Uncomment for PART2
-        # if num == '8':
-        #     r[num] = Parser(r, [['42'], ['42', '8']])
-        # elif num == '11':
-        #     r[num] = Parser(r, [['42', '31'], ['42', '11', '31']])
+    #Uncomment for PART2
+    # r['8'] = Parser(r, [['42'], ['42', '8']])
+    # r['11'] = Parser(r, [['42', '31'], ['42', '11', '31']])
 
     ans1 = 0
     for mess in messages:
-        result = r['0'].parse([(True, mess)], 0, len(mess))
+        result = r['0'].parse([(True, mess)], 0, len(allRules))
         if any([m == (True, '') for m in result]):
             ans1 += 1
     print(ans1)
@@ -43,9 +42,7 @@ class SimpleParser():       #Parses a single message returning parsed message or
         for m in messages:
             parsedMessage = self.func(m[1])
             if parsedMessage[0]:
-                validMessages.append(parsedMessage)
-  #     print(message, parsedMessage)
-    
+                validMessages.append(parsedMessage)    
         return validMessages
 
 class Parser():
@@ -58,13 +55,9 @@ class Parser():
         validMessages = []
         if count > limit:
             return validMessages
-
- #       print(self.rules)
         for rule in self.rules:
-#            print(rule) 
             temp = messages
             for num in rule:
-         #       print(num)
                 temp = self.r[num].parse(temp, count, limit)
             validMessages += temp
         return validMessages    
